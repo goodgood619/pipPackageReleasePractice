d9101e480b2424467609d30d004d633304e48931
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2020-04-23 18:28:13 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pipPractice',
+    version='3.0',
+    description='pip install Test',
+    author='goodgood619',
+    author_email='gktgnjftm@naver.com',
+    # url='https://github.com/rampart81/pyquibase',
+    # download_url='https://githur.com/rampart81/pyquibase/archive/1.0.tar.gz',
+    install_requires=[],
+    packages=find_packages(),
+    keywords=['pipPractice'],
+    python_requires='>=3',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
